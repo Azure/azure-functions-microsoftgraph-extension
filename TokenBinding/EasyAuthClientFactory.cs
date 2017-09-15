91d97474e63e8311b336f2d9737e0d5494f60e47
Mike Stall <mikestall@hotmail.com>
2017-09-15 16:19:34 -0700

    Touchups

--- TokenBinding/EasyAuthClientFactory.cs
+++ TokenBinding/EasyAuthClientFactory.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Azure.WebJobs.Host;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.WebJobs.Host;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- TokenBinding/EasyAuthTokenClient.cs
+++ TokenBinding/EasyAuthTokenClient.cs
@@ -1,4 +1,7 @@
-﻿namespace TokenBinding
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace TokenBinding
 {
     using System;
     using System.IdentityModel.Tokens.Jwt;
