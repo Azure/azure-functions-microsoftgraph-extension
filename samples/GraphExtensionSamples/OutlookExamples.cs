1d3705a270a3ec96e67db8d2c3729f1bb33589d2
Connor McMahon <connorm333@gmail.com>
2017-10-27 14:39:42 -0700

    Create attribute examples with links for data type usage

---
+++ samples/GraphExtensionSamples/OutlookExamples.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+namespace GraphExtensionSamples
+{
+    using System.Collections.Generic;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Graph;
+    using Newtonsoft.Json.Linq;
+
+    public static class OutlookExamples
+    {
+        //Sending messages
+
+        public static void SendMailFromMessageObject([Outlook(
+            Identity = TokenIdentityMode.UserFromId,
+            UserId = "sampleuserid")] out Message message)
+        {
+            message = new Message();
+            //See https://github.com/microsoftgraph/msgraph-sdk-dotnet/blob/dev/src/Microsoft.Graph/Models/Generated/Message.cs
+            //for usage
+        }
+
+        public static void SendMailFromJObject([Outlook(
+            Identity = TokenIdentityMode.UserFromId,
+            UserId = "sampleuserid")] out JObject message)
+        {
+            message = new JObject();
+            //See https://developer.microsoft.com/en-us/graph/docs/api-reference/v1.0/resources/message for a json
+            //representation. Note that a "recipient" or "recipients" field takes the place of the ToRecipient field in
+            //the schema, with "recipient" being a single object and "recipients" being multiple
+        }
+
+        public static void SendMailFromPoco([Outlook(
+            Identity = TokenIdentityMode.UserFromId,
+            UserId = "sampleuserid")] out MessagePoco message)
+        {
+            message = new MessagePoco();
+            //See https://developer.microsoft.com/en-us/graph/docs/api-reference/v1.0/resources/message for a json
+            //representation. Note that a "recipient" or "recipients" field takes the place of the ToRecipient field in
+            //the schema, with "recipient" being a single object and "recipients" being multiple
+        }
+
+        public class MessagePoco
+        {
+            public string Subject { get; set; }
+            public string Body { get; set; }
+            public List<RecipientPoco> Recipients { get; set; }
+        }
+
+        public class RecipientPoco
+        {
+            public string Address { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ samples/GraphExtensionSamples/WebhookTriggerExamples.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace GraphExtensionSamples
+{
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Graph;
+
+    public static class WebhookTriggerExamples
+    {
+        //NOTE: There can only be one trigger per resource type. That means all graph webhook subscriptions for the 
+        //given resource type will go through the same function.
+
+        public static void OnMessage([GraphWebhookTrigger(ResourceType = "#Microsoft.Graph.Message")] Message message)
+        {
+            //See https://github.com/microsoftgraph/msgraph-sdk-dotnet/blob/dev/src/Microsoft.Graph/Models/Generated/Message.cs
+            //for usage
+        }
+
+        public static void OnOneDriveChange([GraphWebhookTrigger(ResourceType = "#Microsoft.Graph.DriveItem")] DriveItem driveItem)
+        {
+            //See https://github.com/microsoftgraph/msgraph-sdk-dotnet/blob/dev/src/Microsoft.Graph/Models/Generated/DriveItem.cs
+            //for usage
+        }
+
+        public static void OnContactChange([GraphWebhookTrigger(ResourceType = "#Microsoft.Graph.Contact")] Contact contact)
+        {
+            //See https://github.com/microsoftgraph/msgraph-sdk-dotnet/blob/dev/src/Microsoft.Graph/Models/Generated/Contact.cs
+            //for usage
+        }
+
+        public static void OnEventChange([GraphWebhookTrigger(ResourceType = "#Microsoft.Graph.Event")] Event eventNotice) 
+        {
+            //See https://github.com/microsoftgraph/msgraph-sdk-dotnet/blob/dev/src/Microsoft.Graph/Models/Generated/Event.cs
+            //for usage
+        }
+        }
+}
