c7942cebe2477d33b7a9d8b9df3a36136d84a2ee
Connor McMahon <connorm333@gmail.com>
2017-11-08 11:36:26 -0800

    Add support for POCO binding for subscription entries

--- samples/GraphExtensionSamples/WebhookSubscriptionExamples.cs
+++ samples/GraphExtensionSamples/WebhookSubscriptionExamples.cs
@@ -7,8 +7,9 @@ namespace GraphExtensionSamples
 
     public static class WebhookSubscriptionExamples
     {
+        //Note that this function will only pass if the ClientCredentials identity has permission to refresh all of the available webhooks
         public static void RefreshAllSubscriptions(
-            [GraphWebhookSubscription(Identity = TokenIdentityMode.ClientCredentials)] string[] subIds,
+            [GraphWebhookSubscription] string[] subIds, 
             [GraphWebhookSubscription(
             Action = GraphWebhookSubscriptionAction.Refresh,
             Identity = TokenIdentityMode.ClientCredentials)] ICollector<string> refreshedSubscriptions)
@@ -16,6 +17,43 @@ namespace GraphExtensionSamples
             foreach (var subId in subIds)
             {
                 refreshedSubscriptions.Add(subId);
+            }
+        }
+
+        //This more complicated function without setting up an application identity with lots of permissions
+        public static async void RefreshAllSubscriptionsWithoutClientCredentials(
+            [GraphWebhookSubscription] SubscriptionPoco[] existingSubscriptions,
+            IBinder binder)
+        {
+            foreach (var subscription in existingSubscriptions)
+            {
+                // binding in code to allow dynamic identity
+                var subscriptionsToRefresh = await binder.BindAsync<IAsyncCollector<string>>(
+                    new GraphWebhookSubscriptionAttribute()
+                    {
+                        Action = GraphWebhookSubscriptionAction.Refresh,
+                        Identity = TokenIdentityMode.UserFromId,
+                        UserId = subscription.UserId
+                    }
+                );
+                {
+                    await subscriptionsToRefresh.AddAsync(subscription.Id);
+                }
+            }
+        }
+
+        public static void RefreshSubscriptionsSelectively(
+            [GraphWebhookSubscription] SubscriptionPoco[] existingSubscriptions,
+            [GraphWebhookSubscription(
+            Action = GraphWebhookSubscriptionAction.Refresh,
+            Identity = TokenIdentityMode.ClientCredentials)] ICollector<string> refreshedSubscriptions)
+        {
+            foreach (var subscription in existingSubscriptions)
+            {
+                if(subscription.ChangeType.Equals("updated") && subscription.ODataType.Equals("#Microsoft.Graph.Message"))
+                {
+                    refreshedSubscriptions.Add(subscription.Id);
+                }   
             }
         }
 
@@ -40,5 +78,21 @@ namespace GraphExtensionSamples
         {
             clientState = Guid.NewGuid().ToString();
         }
+
+        public class SubscriptionPoco
+        {
+            public string UserId { get; set; }
+            //All of the below are properties of the Subscription object that can be used in your custom POCO
+            //See https://github.com/microsoftgraph/msgraph-sdk-dotnet/blob/81c50e72166152f9f84dc38b2516379b7a536300/src/Microsoft.Graph/Models/Generated/Subscription.cs
+            //for usage
+            public string Id { get; set; }
+            public string ODataType { get; set; }
+            public string Resource { get; set; }
+            public string ChangeType { get; set; }
+            public string ClientState { get; set; }
+            public string NotificationUrl { get; set; }
+            public DateTimeOffset? ExpirationDateTime { get; set; }
+        }
+        
     }
 }
--- src/MicrosoftGraphBinding/Config/MicrosoftGraphExtensionConfig.cs
+++ src/MicrosoftGraphBinding/Config/MicrosoftGraphExtensionConfig.cs
@@ -17,12 +17,15 @@ namespace Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph
     using Microsoft.Azure.WebJobs;
     using Microsoft.Azure.WebJobs.Extensions.AuthTokens;
     using Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph.Config;
+    using Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph.Config.Converters;
     using Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph.Services;
     using Microsoft.Azure.WebJobs.Host;
     using Microsoft.Azure.WebJobs.Host.Bindings;
     using Microsoft.Azure.WebJobs.Host.Config;
     using Microsoft.Graph;
     using Newtonsoft.Json.Linq;
+    using static Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph.Config.Converters.ExcelConverters;
+    using static Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph.Config.Converters.GraphWebhookSubscriptionConverters;
 
     /// <summary>
     /// WebJobs SDK Extension for O365 Token binding.
@@ -63,47 +66,47 @@ namespace Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph
             var webhookTriggerProvider = new WebhookTriggerBindingProvider();
             _webhookConfig = new GraphWebhookConfig(context.GetWebhookHandler(), subscriptionStore, webhookTriggerProvider);
 
-            var converter = new Converters(_serviceManager, _webhookConfig);
-
-            // Extend token attribute to retrieve [authenticated] GraphServiceClient
-            //this.tokenExtension.TokenRule.BindToInput<GraphServiceClient>(converter);
+            var graphWebhookConverter = new GraphWebhookSubscriptionConverter(_serviceManager, _webhookConfig);
 
             // Webhooks
             var webhookSubscriptionRule = context.AddBindingRule<GraphWebhookSubscriptionAttribute>();
-
-            webhookSubscriptionRule.BindToInput<Subscription[]>(converter);
-            webhookSubscriptionRule.BindToInput<string[]>(converter);
-            webhookSubscriptionRule.BindToInput<JArray>(converter);
+            webhookSubscriptionRule.BindToInput<Subscription[]>(graphWebhookConverter);
+            webhookSubscriptionRule.BindToInput<OpenType[]>(typeof(GenericGraphWebhookSubscriptionConverter<>), _serviceManager, _webhookConfig);
+            webhookSubscriptionRule.BindToInput<string[]>(graphWebhookConverter);
+            webhookSubscriptionRule.BindToInput<JArray>(graphWebhookConverter);
             webhookSubscriptionRule.BindToCollector<string>(CreateCollector);
 
             context.AddBindingRule<GraphWebhookTriggerAttribute>().BindToTrigger(webhookTriggerProvider);
 
             // OneDrive
             var OneDriveRule = context.AddBindingRule<OneDriveAttribute>();
+            var oneDriveConverter = new OneDriveConverter(_serviceManager);
 
             // OneDrive inputs
-            OneDriveRule.BindToInput<byte[]>(converter);
-            OneDriveRule.BindToInput<string>(converter);
-            OneDriveRule.BindToInput<Stream>(converter);
-            OneDriveRule.BindToInput<DriveItem>(converter);
+            OneDriveRule.BindToInput<byte[]>(oneDriveConverter);
+            OneDriveRule.BindToInput<string>(oneDriveConverter);
+            OneDriveRule.BindToInput<Stream>(oneDriveConverter);
+            OneDriveRule.BindToInput<DriveItem>(oneDriveConverter);
 
-            OneDriveRule.BindToCollector<byte[]>(converter.CreateCollector);
+            OneDriveRule.BindToCollector<byte[]>(CreateCollector);
 
             // Excel
             var ExcelRule = context.AddBindingRule<ExcelAttribute>();
 
+            var excelConverter = new ExcelConverter(_serviceManager);
+
             // Excel Outputs
             ExcelRule.AddConverter<object[][], JObject>(ExcelService.CreateRows);
-            ExcelRule.AddConverter<List<OpenType>, JObject>(typeof(GenericConverter<>)); // used to append/update lists of POCOs
-            ExcelRule.AddConverter<OpenType, JObject>(typeof(GenericConverter<>)); // used to append/update arrays of POCOs
-            ExcelRule.BindToCollector<JObject>(converter.CreateCollector);
-            ExcelRule.BindToCollector<JObject>(typeof(POCOConverter<>));
+            ExcelRule.AddConverter<List<OpenType>, JObject>(typeof(GenericExcelRowConverter<>)); // used to append/update lists of POCOs
+            ExcelRule.AddConverter<OpenType, JObject>(typeof(GenericExcelRowConverter<>)); // used to append/update arrays of POCOs
+            ExcelRule.BindToCollector<JObject>(excelConverter.CreateCollector);
+            ExcelRule.BindToCollector<JObject>(typeof(POCOExcelRowConverter<>));
 
             // Excel Inputs
-            ExcelRule.BindToInput<string[][]>(converter);
-            ExcelRule.BindToInput<WorkbookTable>(converter);
-            ExcelRule.BindToInput<List<OpenType>>(typeof(POCOConverter<>), _serviceManager);
-            ExcelRule.BindToInput<OpenType>(typeof(POCOConverter<>), _serviceManager);
+            ExcelRule.BindToInput<string[][]>(excelConverter);
+            ExcelRule.BindToInput<WorkbookTable>(excelConverter);
+            ExcelRule.BindToInput<List<OpenType>>(typeof(POCOExcelRowConverter<>), _serviceManager);
+            ExcelRule.BindToInput<OpenType>(typeof(POCOExcelRowConverter<>), _serviceManager);
 
             // Outlook
             var OutlookRule = context.AddBindingRule<OutlookAttribute>();
@@ -111,7 +114,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph
             // Outlook Outputs
             OutlookRule.AddConverter<JObject, Message>(OutlookService.CreateMessage);
             OutlookRule.AddConverter<string, Message>(OutlookService.CreateMessage);
-            OutlookRule.BindToCollector<Message>(converter.CreateCollector);
+            OutlookRule.BindToCollector<Message>(CreateCollector);
         }
 
         private void ConfigureServiceManager(ExtensionConfigContext context)
@@ -125,10 +128,23 @@ namespace Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph
             }
         }
 
-        public IAsyncCollector<string> CreateCollector(GraphWebhookSubscriptionAttribute attr)
+        private IAsyncCollector<string> CreateCollector(GraphWebhookSubscriptionAttribute attr)
         {
             return new GraphWebhookSubscriptionAsyncCollector(_serviceManager, _log, _webhookConfig, attr);
         }
+
+        private IAsyncCollector<Message> CreateCollector(OutlookAttribute attr)
+        {
+            var service = Task.Run(() => _serviceManager.GetOutlookService(attr)).GetAwaiter().GetResult();
+            return new OutlookAsyncCollector(service);
+        }
+
+        private IAsyncCollector<byte[]> CreateCollector(OneDriveAttribute attr)
+        {
+            var service = Task.Run(() => _serviceManager.GetOneDriveService(attr)).GetAwaiter().GetResult();
+            return new OneDriveAsyncCollector(service, attr);
+        }
+
 
         /// <summary>
         /// HttpRequest -> HttpResponse
@@ -142,231 +158,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph
             var handler = new GraphWebhookSubscriptionHandler(_serviceManager, _webhookConfig, _log);
             var response = await handler.ProcessAsync(input);
             return response;
-        }
-
-        /// <summary>
-        /// Used to convert POCOs to JObjects (for Excel output bindings)
-        /// T -> used to append a row
-        /// T[] -> used to update a table
-        /// </summary>
-        /// <typeparam name="T">Generic POCO type</typeparam>
-        public class GenericConverter<T> : IConverter<List<T>, JObject>, IConverter<T, JObject>
-        {
-            /// <summary>
-            /// Convert from POCO -> JObject (either row or rows)
-            /// </summary>
-            /// <param name="input">POCO input from fx</param>
-            /// <returns>JObject with proper keys set</returns>
-            public JObject Convert(T input)
-            {
-                // handle T[]
-                if (typeof(T).IsArray)
-                {
-                    var array = input as object[];
-                    return ConvertEnumerable(array);
-                }
-                else
-                {
-                    // handle T
-                    JObject data = JObject.FromObject(input);
-                    data[O365Constants.POCOKey] = true; // Set Microsoft.O365Bindings.POCO flag to indicate that data is from POCO (vs. object[][])
-
-                    return data;
-                }
-            }
-
-            /// <summary>
-            /// Convert from List<POCO> -> JObject
-            /// </summary>
-            /// <param name="input">POCO input from fx</param>
-            /// <returns>JObject with proper keys set</returns>
-            public JObject Convert(List<T> input)
-            {
-                return ConvertEnumerable(input);
-            }
-
-            private JObject ConvertEnumerable<U>(IEnumerable<U> input)
-            {
-                JObject jsonContent = new JObject();
-
-                JArray rowData = JArray.FromObject(input);
-
-                // List<T> -> JArray
-                jsonContent[O365Constants.ValuesKey] = rowData;
-
-                // Set rows, columns needed if updating entire worksheet
-                jsonContent[O365Constants.RowsKey] = rowData.Count();
-
-                // No exception -- array is rectangular by default
-                jsonContent[O365Constants.ColsKey] = rowData.First.Count();
-
-                // Set POCO key to indicate that the values need to be ordered to match the header of the existing table
-                jsonContent[O365Constants.POCOKey] = true;
-
-                return jsonContent;
-            }
-        }
-
-        /// <summary>
-        /// Used for INPUT bindings: convert Excel Attribute -> POCO inputs
-        /// </summary>
-        /// <typeparam name="T">POCO type user wishes to bind Excel contents to</typeparam>
-        internal class POCOConverter<T> : IAsyncConverter<ExcelAttribute, T[]>, IAsyncConverter<ExcelAttribute, List<T>>
-            where T : new()
-        {
-            private readonly ServiceManager _serviceManager;
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="POCOConverter{T}"/> class.
-            /// </summary>
-            /// <param name="parent">O365Extension to which the result of the request for data will be returned</param>
-            public POCOConverter(ServiceManager serviceManager)
-            {
-                this._serviceManager = serviceManager;
-            }
-
-            async Task<List<T>> IAsyncConverter<ExcelAttribute, List<T>>.ConvertAsync(ExcelAttribute input, CancellationToken cancellationToken)
-            {
-                var manager = await _serviceManager.GetExcelService(input);
-                return await manager.GetExcelRangePOCOListAsync<T>(input);
-            }
-
-            async Task<T[]> IAsyncConverter<ExcelAttribute, T[]>.ConvertAsync(ExcelAttribute input, CancellationToken cancellationToken)
-            {
-                var manager = await _serviceManager.GetExcelService(input);
-                return await manager.GetExcelRangePOCOAsync<T>(input);
-            }
-
-            public IAsyncCollector<JObject> CreateCollector(ExcelAttribute attr)
-            {
-                var manager = Task.Run(() => _serviceManager.GetExcelService(attr)).GetAwaiter().GetResult();
-                return new ExcelAsyncCollector(manager, attr);
-            }
-        }
-
-        /// <summary>
-        /// Used for input bindings; Attribute -> Input type
-        /// </summary>
-        internal class Converters :
-            IAsyncConverter<ExcelAttribute, string[][]>,
-            IAsyncConverter<ExcelAttribute, WorkbookTable>,
-            IAsyncConverter<OneDriveAttribute, byte[]>,
-            IAsyncConverter<OneDriveAttribute, string>,
-            IAsyncConverter<OneDriveAttribute, Stream>,
-            IAsyncConverter<OneDriveAttribute, DriveItem>,
-            IAsyncConverter<GraphWebhookSubscriptionAttribute, Subscription[]>,
-            IAsyncConverter<GraphWebhookSubscriptionAttribute, string[]>,
-            IAsyncConverter<GraphWebhookSubscriptionAttribute, JArray>
-        {
-            private readonly ServiceManager _serviceManager;
-            private readonly GraphWebhookConfig _webhookConfig;
-
-            public Converters(ServiceManager parent, GraphWebhookConfig webhookConfig)
-            {
-                _serviceManager = parent;
-                _webhookConfig = webhookConfig;
-            }
-
-            public IAsyncCollector<JObject> CreateCollector(ExcelAttribute attr)
-            {
-                var service = Task.Run(() => _serviceManager.GetExcelService(attr)).GetAwaiter().GetResult();
-                return new ExcelAsyncCollector(service, attr);
-            }
-
-            public IAsyncCollector<byte[]> CreateCollector(OneDriveAttribute attr)
-            {
-                var service = Task.Run(() => _serviceManager.GetOneDriveService(attr)).GetAwaiter().GetResult();
-                return new OneDriveAsyncCollector(service, attr);
-            }
-
-            public IAsyncCollector<Message> CreateCollector(OutlookAttribute attr)
-            {
-                var service = Task.Run(() => _serviceManager.GetOutlookService(attr)).GetAwaiter().GetResult();
-                return new OutlookAsyncCollector(service);
-            }
-
-            async Task<string[][]> IAsyncConverter<ExcelAttribute, string[][]>.ConvertAsync(ExcelAttribute attr, CancellationToken cancellationToken)
-            {
-                var service = await _serviceManager.GetExcelService(attr);
-                return await service.GetExcelRangeAsync(attr);
-            }
-
-            async Task<WorkbookTable> IAsyncConverter<ExcelAttribute, WorkbookTable>.ConvertAsync(ExcelAttribute input, CancellationToken cancellationToken)
-            {
-                var service = await _serviceManager.GetExcelService(input);
-                return await service.GetExcelTable(input);
-            }
-
-            async Task<byte[]> IAsyncConverter<OneDriveAttribute, byte[]>.ConvertAsync(OneDriveAttribute input, CancellationToken cancellationToken)
-            { 
-                var service = await _serviceManager.GetOneDriveService(input);
-                return await service.GetOneDriveContentsAsByteArrayAsync(input);
-            }
-
-            async Task<string> IAsyncConverter<OneDriveAttribute, string>.ConvertAsync(OneDriveAttribute input, CancellationToken cancellationToken)
-            {
-                var service = await _serviceManager.GetOneDriveService(input);
-                var byteArray = await service.GetOneDriveContentsAsByteArrayAsync(input);
-                return Encoding.UTF8.GetString(byteArray);
-            }
-
-            async Task<Stream> IAsyncConverter<OneDriveAttribute, Stream>.ConvertAsync(OneDriveAttribute input, CancellationToken cancellationToken)
-            {
-                var service = await _serviceManager.GetOneDriveService(input);
-                return await service.GetOneDriveContentsAsStreamAsync(input);
-            }
-
-            async Task<DriveItem> IAsyncConverter<OneDriveAttribute, DriveItem>.ConvertAsync(OneDriveAttribute input, CancellationToken cancellationToken)
-            {
-                var service = await _serviceManager.GetOneDriveService(input);
-                return await service.GetOneDriveItemAsync(input);
-            }
-
-            async Task<Subscription[]> IAsyncConverter<GraphWebhookSubscriptionAttribute, Subscription[]>.ConvertAsync(GraphWebhookSubscriptionAttribute input, CancellationToken cancellationToken)
-            {
-                return await GetSubscriptionsFromAttribute(input);
-            }
-
-            async Task<string[]> IAsyncConverter<GraphWebhookSubscriptionAttribute, string[]>.ConvertAsync(GraphWebhookSubscriptionAttribute input, CancellationToken cancellationToken)
-            {
-                Subscription[] subscriptions = await GetSubscriptionsFromAttribute(input);
-                return subscriptions.Select(sub => sub.Id).ToArray();
-            }
-
-            async Task<JArray> IAsyncConverter<GraphWebhookSubscriptionAttribute, JArray>.ConvertAsync(GraphWebhookSubscriptionAttribute input, CancellationToken cancellationToken)
-            {
-                Subscription[] subscriptions = await GetSubscriptionsFromAttribute(input);
-                var serializedSubscriptions = new JArray();
-                foreach (var subscription in subscriptions)
-                {
-                    serializedSubscriptions.Add(JObject.FromObject(subscription));
-                }
-                return serializedSubscriptions;
-            }
-
-            private async Task<Subscription[]> GetSubscriptionsFromAttribute(GraphWebhookSubscriptionAttribute attribute)
-            {
-                IEnumerable<SubscriptionEntry> subscriptionEntries = await _webhookConfig.SubscriptionStore.GetAllSubscriptionsAsync();
-                if (TokenIdentityMode.UserFromRequest.ToString().Equals(attribute.Filter, StringComparison.OrdinalIgnoreCase))
-                {
-                    var dummyTokenAttribute = new TokenAttribute()
-                    {
-                        Resource = O365Constants.GraphBaseUrl,
-                        Identity = TokenIdentityMode.UserFromToken,
-                        UserToken = attribute.UserToken,
-                        IdentityProvider = "AAD",
-                    };
-                    var graph = await _serviceManager.GetMSGraphClientAsync(dummyTokenAttribute);
-                    var user = await graph.Me.Request().GetAsync();
-                    subscriptionEntries = subscriptionEntries.Where(entry => entry.UserId.Equals(user.Id));
-                }
-                else if (attribute.Filter != null)
-                {
-                    throw new InvalidOperationException($"There is no filter for {attribute.Filter}");
-                }
-
-                return subscriptionEntries.Select(entry => entry.Subscription).ToArray();
-            }
         }
     }
 }
