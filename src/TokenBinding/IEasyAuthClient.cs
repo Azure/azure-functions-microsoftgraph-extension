dda44090bb2d4206e22269fda5f943d5ad4b9792
Connor McMahon <connorm333@gmail.com>
2018-09-10 20:41:09 -0700

    Update the extension to have strong-name signing

--- src/TokenBinding/IEasyAuthClient.cs
+++ src/TokenBinding/IEasyAuthClient.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
-using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 namespace Microsoft.Azure.WebJobs.Extensions.AuthTokens
 {
     using System.IdentityModel.Tokens.Jwt;
