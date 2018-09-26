2412619c8190c585a4de639efa29773da891e3fa
Vincent Biret <vincentbiret@hotmail.com>
2018-09-26 10:51:58 -0400

    fixed: binding never loading token map location from configuration

--- src/MicrosoftGraphBinding/Config/GraphOptions.cs
+++ src/MicrosoftGraphBinding/Config/GraphOptions.cs
@@ -7,20 +7,20 @@ namespace Microsoft.Azure.WebJobs.Extensions.MicrosoftGraph.Config
 {
     public class GraphOptions
     {
+        private const string defaultTokenMapLocation = "D:/home/data/byob_graphmap";
         public string SubscriptionStoreLocationAppSettingName { get; set; } = "BYOB_TokenMap";
 
         public string GraphBaseUrl { get; set; } = O365Constants.GraphBaseUrl;
 
-        public string TokenMapLocation { get; set; } = "D:/home/data/byob_graphmap";
+        public string TokenMapLocation { get; set; } = defaultTokenMapLocation;
 
         public TimeSpan WebhookExpirationTimeSpan { get; set; } = new TimeSpan(0, 0, 4230, 0);
 
         public void SetAppSettings(INameResolver appSettings)
         {
-            if (TokenMapLocation == null)
-            {
-                TokenMapLocation = appSettings.Resolve(SubscriptionStoreLocationAppSettingName);
-            }
+            var settingsTokenMapLocation = appSettings.Resolve(SubscriptionStoreLocationAppSettingName);
+            if (!string.IsNullOrEmpty(settingsTokenMapLocation) && TokenMapLocation == defaultTokenMapLocation)
+                TokenMapLocation = settingsTokenMapLocation;
         }
     }
 }
